67bff2d0a9e1177c8322a3e831c45378d50e6a4d
Bharat Makwana <bharatmk257@gmail.com>
2021-10-03 10:31:41 +0530

    added token file

--- lib/data/dataproviders/network/weather_api.dart
+++ lib/data/dataproviders/network/weather_api.dart
@@ -1,12 +1,14 @@
 import 'package:weather_bloc/data/model/weather_model.dart';
 import 'package:dio/dio.dart';
 
+import '../../../token.dart';
+
 class WeatherApi {
   static Future<Weather?> getWeather(String place) async {
     try {
-      const token = "d27c49560b2aaf8a9303a7f8801a96f3";
+      
       String requestUrl =
-          "https://api.openweathermap.org/data/2.5/weather?q=$place&appid=$token";
+          "https://api.openweathermap.org/data/2.5/weather?q=$place&appid=$TOKEN";
       Response response = await Dio().get(requestUrl);
       if (response.statusCode == 200) {
         return weatherFromJson(response.toString());
